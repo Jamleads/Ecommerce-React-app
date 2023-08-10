9bcd39e4503058708819c20229572ca7253c3554
AdetunjiJakes <68664742+Jamleads@users.noreply.github.com>
2023-08-10 06:44:05 +0100

    Chaeckout Thank you message

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -4,9 +4,12 @@ import Shocase from "./Showcase";
 import ProductCart2 from "./ProductCart2";
 import Button from "./Button";
 import { useDispatch } from "react-redux";
-import { remove } from "../store/cartSlice";
-import { clear } from "../store/cartSlice";
+import { remove, clear } from "../store/cartSlice";
 import { toast, ToastContainer } from "react-toastify";
+import { Link } from "react-router-dom";
+import clockImg from "/assets/clock-icon.svg";
+import checklist from "/assets/checklist.svg";
+import checked from "/assets/check-mark-icon.svg";
 
 const Cart = () => {
   const productCart = useSelector((state) => state.cart);
@@ -43,11 +46,21 @@ const Cart = () => {
   const chargesFee = itemsTotalPrice * 0.02;
   const checoutTotalPrice = itemsTotalPrice + chargesFee;
 
+  const chechOut = () => {
+    const modal = document.querySelector(".modal");
+    const cartSection = document.querySelector(".cartSection");
+    const checkoutBtn = document.querySelector(".checkoutBtn");
+    checkoutBtn.addEventListener("click", () => {
+      cartSection.classList.add("blur");
+      modal.classList.remove("hidden");
+    });
+  };
+
   return (
-    <>
+    <div className="relative">
       <Shocase pageTitle="Shoping Cart" />
 
-      <section className="lg:w-[70%] mx-auto mt-32 lg:flex gap-10">
+      <section className="cartSection lg:w-[70%] mx-auto mt-32 lg:flex gap-10">
         <div className="lg:w-2/3 left">
           <div className="flex items-center lg:mb-8">
             <div className="product-side w-2/5 lg:text-lg text-[#101750] font-bold">
@@ -77,8 +90,10 @@ const Cart = () => {
           ))}
 
           <div className="cart-btns flex items-center justify-between my-5">
-            <Button buttonText="Update Cart" to="/" />
-            <Button buttonText="Clear Cart" />
+            <Link to="/">
+              <Button buttonText="Update Cart" />
+            </Link>
+            <Button buttonText="Clear Cart" onClick={() => clearCart()} />
           </div>
         </div>
 
@@ -112,20 +127,41 @@ const Cart = () => {
                 </p>
               </div>
 
-              <div>{/* visa, mastercard and so on.... */}</div>
-
               <Button
-                style="w-full bg-[#19D16F] mt-10 mb-5"
+                style="checkoutBtn w-full bg-[#19D16F] mt-10 mb-5"
                 buttonText="Proceed To Checkout"
-                onClick={() => clearCart()}
+                onClick={() => chechOut()}
               />
             </div>
           </div>
         </div>
       </section>
 
+      <div className="absolute modal top-[35%] left-0 right-0 hidden w-full flex justify-center">
+        <div className="lg:w-[50%] lg:h-[350px] mx-auto p-5 relative flex flex-col items-center justify-center gap-5 rounded-xl border-4 border-dashed bg-mainWhite">
+          <div className="absolute top-0 -left-8">
+            <img src={clockImg} alt="clock-image" width="70px" />
+          </div>
+
+          <div className="absolute bottom-0 -right-8">
+            <img src={checklist} alt="checklist-image" width="70px" />
+          </div>
+
+          <img src={checked} alt="check-mark-icon" />
+          <h1 className="lg:text-3xl font-bold">Your Order is Completed!</h1>
+          <p className="text-center font-light w-[80%]">
+            Thank you for your order! Your delivery package is being processed
+            and will be ready within 3-6 hours. You will receive an email
+            confirmation when your delivery package is ready for pick up.
+          </p>
+          <Link to="/">
+            <Button buttonText="Continue Shopping" style="text-sm" />
+          </Link>
+        </div>
+      </div>
+
       <ToastContainer />
-    </>
+    </div>
   );
 };
 
