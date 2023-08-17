2ed92a9a4a718287db6543af017a6d77252f0cb0
AdetunjiJakes <68664742+Jamleads@users.noreply.github.com>
2023-08-17 10:58:16 +0100

    wishlist condition

--- src/Components/Wishlist.jsx
+++ src/Components/Wishlist.jsx
@@ -2,24 +2,38 @@ import React from "react";
 import ProductDetailsCard from "./ProductDetailsCard";
 import { useSelector } from "react-redux";
 import Showcase from "./Showcase";
+import { Link } from "react-router-dom";
+import Footer from "./Footer";
 
 const Wishlist = () => {
   const productsFavourites = useSelector((state) => state.favourite);
 
   return (
-    <div>
+    <>
       <Showcase pageTitle="Wishlist" />
 
       <div className="lg:w-[70%] mx-auto lg:mt-32 mt-10 my-20 grid lg:grid-cols-1 grid-cols-1 gap-y-10">
-        {productsFavourites.map((product) => {
-          return (
-            <div key={product.id}>
-              <ProductDetailsCard {...product} favStyle="hidden" />
-            </div>
-          );
-        })}
+        {productsFavourites.length ? (
+          productsFavourites.map((product) => {
+            return (
+              <div key={product.id}>
+                <ProductDetailsCard {...product} favStyle="hidden" />
+              </div>
+            );
+          })
+        ) : (
+          <p className="text-center text-2xl">
+            You haven't added any product to wishlist, yet kindly proceed to{" "}
+            <Link to="/" className="text-pink font-bold">
+              Home
+            </Link>{" "}
+            and select products
+          </p>
+        )}
       </div>
-    </div>
+
+      <Footer />
+    </>
   );
 };
 
